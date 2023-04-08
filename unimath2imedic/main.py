0aba71db42ba7b98916aa49c5a5a693cb7a8020c
Nori Hayashi <net@hayashi-lin.net>
2023-04-08 13:33:04 +0900

    [add] shortened names for specific characters

--- unimath2imedic/main.py
+++ unimath2imedic/main.py
@@ -1,15 +1,24 @@
 import codecs
+from dataclasses import dataclass
 import re
 import typing
 from requests_cache.session import CachedSession
-
 from . import roman2kana
+
+@dataclass(frozen=True, slots=True)
+class CharInfo:
+    char: str
+    name: str
+    type: str
+    description: str
 
 _re_ITEM = re.compile(
     r'^\\UnicodeMathSymbol\{"(?P<point>.{5})\}\{\\(?P<name>\S+)\s*\}\{\\(?P<type>\S+)\}\{(?P<desc>.*?)\}'
 )
 
-def generate(out: typing.BinaryIO):
+_re_PREFIX_TO_STRIP = re.compile(r"^(mup|mdlgwht|mdlgblk)(?P<name>.*)")
+
+def gen_mappging() -> typing.Iterator[CharInfo]:
     session = CachedSession(
         "unicode-math-to-imedic",
         backend = "filesystem",
@@ -20,18 +29,34 @@ def generate(out: typing.BinaryIO):
         stream = True
     )
 
-    out.write(codecs.BOM_UTF16_LE)
     for line in map(lambda b: b.decode("utf-8"), r.iter_lines()):
         if match := _re_ITEM.match(line):
-            char = chr(int(match.group("point"), base = 16))
-            name = match.group("name")
-            yomi = roman2kana.roman2kana_msime(
-                name.lower()
+            info = CharInfo(
+                char = chr(int(match.group("point"), base = 16)),
+                name = match.group("name"),
+                type = match.group('type'),
+                description = match.group('desc'),
             )
-            
-            # description length limit: 128 characters
-            description = f"unicode-math symbol: \\{name} 【{match.group('type')}】 {match.group('desc')}"[:128]
+            yield info
 
-            out.write(
-                f"{yomi}\t{char}\t顔文字\t{description}\r\n".encode("utf-16-le")
-            )
+            if match := _re_PREFIX_TO_STRIP.match(info.name):
+                yield CharInfo(
+                    char = info.char,
+                    name = match.group("name"),
+                    type = info.type,
+                    description = info.description,
+                )
+
+def generate(out: typing.BinaryIO):
+    out.write(codecs.BOM_UTF16_LE)
+    for info in gen_mappging():
+        yomi = roman2kana.roman2kana_MSIME(
+            info.name.lower()
+        )
+        
+        # description length limit: 128 characters
+        description = f"unicode-math symbol: \\{info.name} 【{info.type}】 {info.description}"[:128]
+
+        out.write(
+            f"{yomi}\t{info.char}\t顔文字\t{description}\r\n".encode("utf-16-le")
+        )
